298f90e017c0a8f5453620909e9d60df8bd500be
James Winchester <wildrover254@gmail.com>
2021-08-01 21:58:18 -0400

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import {
+    BroswerRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom'
 import Search from './Search';
 import Nav from './Nav';
 import PhotoContainer from './PhotoContainer';
